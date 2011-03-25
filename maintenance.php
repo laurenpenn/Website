4d64809c78a49873e5828171d32cd15ead248f35
Patrick Daly <patrick@developdaly.com>
2011-03-25 03:21:24 +0000

    Updated design for maintenance.php and includes Facebook like box.

--- maintenance.php
+++ maintenance.php
@@ -13,32 +13,66 @@ header( 'Content-Type: text/html; charset=utf-8' );
     
 	<style type="text/css">
 	body {
-		background: #15100e;
+		background: #000;
 		color: #5c5650;
 		font: 14px/18px Tahoma, Arial, Helvetica, sans-serif;
+		margin: 50px 0;
 	}
 	
+	#logo { text-align: center; }
+	
 	#ohno {
-		background: #ccc;
-		border: 1px solid #fff;
+		border: 1px solid #000;
 		border-radius: 20px;
 		-moz-border-radius: 20px;
 		-webkit-border-radius: 20px;
-		box-shadow: 0 0 10px #000;
-		-moz-box-shadow: 0 0 10px #000;
-		-webkit-box-shadow: 0 0 10px #000;
-		margin: 0 auto;
-		padding: 50px;
-		width: 500px;
+		box-shadow: 0 0 30px #38241D;
+		-moz-box-shadow: 0 0 30px #38241D;
+		-webkit-box-shadow: 0 0 30px #38241D;
+		margin: 50px auto 0;
+		width: 550px;
 	}
+	#ohno-inside {
+		background: #15100e url('wp-content/themes/dbc/library/images/bg.png') center top no-repeat;
+		border-radius: 20px;
+		-moz-border-radius: 20px;
+		-webkit-border-radius: 20px;
+		box-shadow: inset 0 0 10px #000;
+		-moz-box-shadow: inset 0 0 10px #000;
+		-webkit-box-shadow: inset 0 0 10px #000;
+		color: #C8B6A2;
+		padding: 30px 50px;
+		text-shadow: -1px -1px 1px #15100e;
+	}
+	h1 {
+		font: 30px/30px "Georgia", "Times New Roman", Times, Serif;
+		text-align: center;
+	}
+	h2 {
+		color: #6B4012;
+		font: 20px/20px "Georgia", "Times New Roman", Times, Serif;
+		text-align: center;
+		text-shadow: 0 1px 0 #000;
+	}
+	p {
+		font-size: 12px;
+	}
+	a { color: #fff; }
 	</style>
 </head>
 <body>
 	
+	<div id="logo"><img src="http://dentonbible.org/wp-content/themes/dbc/library/images/denton-bible-church.png" alt="Denton Bible Church" /></div>
+	
 	<div id="ohno">
-		<h1>Update in progress.</h1>
-		<p>We're doing some pretty awesome things behind the scenes right now. You'd be impressed. Come back real soon.</p>
-		<p><a href="http://dbcmedia.org">DBC</a> Media is still up and running and you can visit us on <a href="http://facebook.com/dentonbible">Facebook</a>.</p>
+		<div id="ohno-inside">
+			<h1>Someone turned off the lights!</h1>
+			<h2>actually, we're just updating the site</h2>
+			<p>We're doing some pretty awesome things behind the scenes right now. You'd be impressed. Come back real soon.</p>
+			<p><a href="http://dbcmedia.org">DBC Media</a> is still up and running.</p>
+			<p>And of course you can always find us on Facebook...</p>
+			<iframe src="http://www.facebook.com/plugins/likebox.php?href=http%253A%252F%252Fwww.facebook.com%252Fpages%252FDenton-TX%252FDenton-Bible-Church%252F206268862487&amp;width=450&amp;colorscheme=light&amp;connections=0&amp;stream=true&amp;header=0&amp;height=395" scrolling="no" frameborder="0" style="background: #fff; border:none; overflow:hidden; width:450px; height:395px;" allowTransparency="true"></iframe>
+		</div>
 	</div>
 </body>
 </html>
