6931ce9d86ea10297d27dbf97eae62937b9ec088
Patrick Daly <patrick@developdaly.com>
2011-05-18 05:30:15 +0000

    First draft of dbc-children theme

--- themes/dbc-children/header.php
+++ themes/dbc-children/header.php
@@ -6,15 +6,14 @@
  * somewhere near the top of the file. It is used mostly as an opening wrapper, which is closed with the 
  * footer.php file. It also executes key functions needed by the theme, child themes, and plugins. 
  *
- * @package DBC
+ * @package Prototype
  * @subpackage Template
  */
 ?>
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>xmlns:fb="http://www.facebook.com/2008/fbml">
+<html <?php language_attributes(); ?>>
 <head>
 <meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
-<meta property="fb:page_id" content="206268862487" />
 <title><?php hybrid_document_title(); ?></title>
 
 <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="all" />
@@ -22,47 +21,43 @@
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
 <?php wp_head(); // wp_head ?>
-		
+
 </head>
 
 <body class="<?php hybrid_body_class(); ?>">
 
-	<?php do_atomic( 'open_body' ); // dbc_children_open_body ?>
+	<?php do_atomic( 'open_body' ); // dbc_open_body ?>
 	
 	<div id="container">
 	
 		<div id="header">
 
-			<?php do_atomic( 'open_header' ); // dbc_children_open_header ?>
+			<?php do_atomic( 'open_header' ); // prototype_open_header ?>
 
 			<div class="wrap">
 
 				<div id="branding">
-					<?php hybrid_site_title(); ?>
+					<?php dbc_child_site_title(); ?>
 				</div><!-- #branding -->
 
 				<?php get_sidebar( 'header' ); // Loads the sidebar-header.php template. ?>
-				
-				<?php get_template_part( 'menu', 'primary' ); // Loads the menu-secondary.php template. ?>
-				
-				<div class="clear"></div>
-				
-				<?php do_atomic( 'header' ); // dbc_children_header ?>
+
+				<?php do_atomic( 'header' ); // prototype_header ?>
 
 			</div><!-- .wrap -->
 
-			<?php do_atomic( 'close_header' ); // dbc_children_close_header ?>
+			<?php do_atomic( 'close_header' ); // prototype_close_header ?>
 
 		</div><!-- #header -->
 
-		<?php do_atomic( 'after_header' ); // dbc_children_after_header ?>
+		<?php do_atomic( 'after_header' ); // prototype_after_header ?>
 
-		
+		<?php get_template_part( 'menu', 'primary' ); // Loads the menu-secondary.php template. ?>
 
-		<?php do_atomic( 'before_main' ); // dbc_children_before_main ?>
+		<?php do_atomic( 'before_main' ); // prototype_before_main ?>
 
 		<div id="main">
 
 			<div class="wrap">
 
-			<?php do_atomic( 'open_main' ); // dbc_children_open_main ?>
+			<?php do_atomic( 'open_main' ); // prototype_open_main ?>
--- themes/dbc-children/page-template-home.php
+++ themes/dbc-children/page-template-home.php
@@ -3,79 +3,89 @@
  * Template Name: Home
  *
  * This template is for the Home page
+ * tes
  *
- * @package DBC
+ * @package Hybrid
  * @subpackage Template
  */
+global $blog_ID;
 
 get_header(); ?>
 
 	<?php do_atomic( 'before_content' ); // dbc_before_content ?>
+	
+	<div id="slider-container-container">
+	<?php get_template_part( 'slider-home' ); // loads slider-home.php ?>
+	</div>
+	
+	<?php get_sidebar( 'home' ); ?>
+	
+	<div class="clear"></div>
 
+	<div id="welcome-bar">
+		<dl id="welcome-preschool">
+			<a href="#">
+				<dt><strong>Preschool</strong></dt>
+				<dd>4 months - 4 years</dd>
+			</a>
+		</dl>
+		<dl id="welcome-elementary">
+			<a href="#">
+				<dt><strong>Elementary</strong></dt>
+				<dd>Kindergarten - 4th grade</dd>
+			</a>
+		</dl>
+		<dl id="welcome-volunteer">
+			<a href="#">
+				<dt><strong>Volunteers</strong></dt>
+				<dd>Learn how to get involved</dd>
+			</a>
+		</dl>
+		<dl id="welcome-connect">
+			<a href="#">
+				<dt><strong>Connect</strong></dt>
+				<dd>Sign up for events</dd>
+			</a>
+		</dl>
+	</div>
+	
+	<div class="clear"></div>
+	
 	<div id="content">
 
-		<?php do_atomic( 'open_content' ); // dbc_open_content ?>
+		<?php do_atomic( 'open_content' ); // prototype_open_content ?>
 
-		<div id="features">
+		<?php if ( hybrid_get_setting( 'latest-message' ) == 'true' ) get_template_part( 'latest-message' ); // loads latest-message.php ?>
+
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+			<div id="post-<?php the_ID(); ?>" class="<?php hybrid_entry_class(); ?>">
+
+				<div class="entry-content">
+					<?php the_content(); ?>
+					<?php wp_link_pages( array( 'before' => '<p class="page-links pages">' . __( 'Pages:', 'hybrid' ), 'after' => '</p>' ) ); ?>
+				</div><!-- .entry-content -->
+
+			</div><!-- .hentry -->
+
+			<?php do_atomic( 'after_singular' ); // prototype_after_singular ?>
+
+			<?php endwhile; ?>
+
+		<?php else: ?>
+
+			<p class="no-data">
+				<?php _e( 'Apologies, but no results were found.', 'hybrid' ); ?>
+			</p><!-- .no-data -->
+
+		<?php endif; ?>
 			
-			<ul>
-				<li class="preschool">
-					<a href="http://children.dentonbible.org/connect/sunday-mornings/preschool/">Preschool</a>
-				</li>
-				<li class="elementary">
-					<a href="http://children.dentonbible.org/connect/sunday-mornings/elementary/">Elementary</a>
-				</li>
-				<li class="curriculum">
-					<a href="http://children.dentonbible.org/connect/our-curriculum/">Curriculum</a>
-				</li>
-				<li class="volunteer">
-					<a href="http://children.dentonbible.org/contact-us/volunteer/">Volunteer</a>
-				</li>
-			</ul>
-						
-		</div>
-
-		<div class="hfeed">
-
-			<?php if ( have_posts() ) : ?>
-
-				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php do_atomic( 'before_entry' ); // dbc_before_entry ?>
-
-					<div id="post-<?php the_ID(); ?>" class="<?php hybrid_entry_class(); ?>">
-
-						<?php do_atomic( 'open_entry' ); // dbc_open_entry ?>
-
-						<div class="entry-content">
-							<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', hybrid_get_textdomain() ) ); ?>
-							<?php wp_link_pages( array( 'before' => '<p class="page-links">' . __( 'Pages:', hybrid_get_textdomain() ), 'after' => '</p>' ) ); ?>
-						</div><!-- .entry-content -->
-
-						<?php echo apply_atomic_shortcode( 'entry_meta', '<div class="entry-meta">[entry-edit-link]</div>' ); ?>
-
-						<?php do_atomic( 'close_entry' ); // dbc_close_entry ?>
-
-					</div><!-- .hentry -->
-
-					<?php do_atomic( 'after_entry' ); // dbc_after_entry ?>
-
-					<?php get_sidebar( 'after-singular' ); // Loads the sidebar-after-singular.php template. ?>
-
-					<?php do_atomic( 'after_singular' ); // dbc_after_singular ?>
-
-				<?php endwhile; ?>
-
-			<?php endif; ?>
-
-		</div><!-- .hfeed -->
-				
-		<?php do_atomic( 'close_content' ); // dbc_close_content ?>
+		<?php do_atomic( 'close_content' ); // prototype_close_content ?>
 
 	</div><!-- #content -->
-		
-	<?php get_sidebar( 'home' ); ?>
-
-	<?php do_atomic( 'after_content' ); // dbc_after_content ?>
+	
+	<div class="clear"></div>
+	
+	<?php do_atomic( 'after_content' ); // prototype_after_content ?>
 
 <?php get_footer(); // Loads the footer.php template. ?>
