41eb0bf92bf5e65ea6d73c701bd2d7671e144cab
Patrick Daly <patrick@developdaly.com>
2011-06-28 19:02:56 +0000

    Checks the DBC Media RSS feed for errors and degrades gracefully if its broken.

--- themes/dbc/latest-message.php
+++ themes/dbc/latest-message.php
@@ -16,22 +16,24 @@
 	
 		include_once(ABSPATH . WPINC . '/feed.php');               // include the required file
 		$feed = fetch_feed('http://dbcmedia.org/sermons/feed/'); // specify the source feed
-	
-		$limit = $feed->get_item_quantity(1); // specify number of items
-		$items = $feed->get_items(0, $limit); // create an array of items
-	
+
+		if (!is_wp_error( $feed ) ) : // Checks that the object is created correctly 
+
+			$limit = $feed->get_item_quantity(1); // specify number of items		
+			$items = $feed->get_items(0, $limit); // create an array of items
+		endif;
 	}
 	if ($limit == 0) echo '<div>The feed is either empty or unavailable.</div>';
 	else foreach ($items as $item) : ?>
-			<div class="latest-message-left">
-				<div class="latest-message-message-title"><a href="<?php echo $item->get_permalink(); ?>" rel="external"><?php echo $item->get_title(); ?></a></div>
-				<div class="latest-message-message-date"><?php echo $item->get_date('F j'); ?></div>
-			</div>
-			
-			<div class="latest-message-right">
-				<div class="latest-message-podcast"><a href="http://itunes.apple.com/podcast/denton-bible-church-sunday/id335635432" rel="external">DBC Podcast</a></div>					
-				<div class="latest-message-listen"><a href="<?php echo $item->get_permalink(); ?>" rel="external">Listen</a></div>
-			</div>
+		<div class="latest-message-left">
+			<div class="latest-message-message-title"><a href="<?php echo $item->get_permalink(); ?>" rel="external"><?php echo $item->get_title(); ?></a></div>
+			<div class="latest-message-message-date"><?php echo $item->get_date('F j'); ?></div>
+		</div>
+		
+		<div class="latest-message-right">
+			<div class="latest-message-podcast"><a href="http://itunes.apple.com/podcast/denton-bible-church-sunday/id335635432" rel="external">DBC Podcast</a></div>					
+			<div class="latest-message-listen"><a href="<?php echo $item->get_permalink(); ?>" rel="external">Listen</a></div>
+		</div>
 	<?php endforeach; ?>
 	
 </div>
