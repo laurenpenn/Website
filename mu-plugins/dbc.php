fd26b95e43cfbe117cdd27b00c57f900568824fb
Patrick Daly <patrick@developdaly.com>
2011-10-06 14:37:52 +0000

    Google Analytics script updated to track all domains from a single GA account.

--- mu-plugins/dbc.php
+++ mu-plugins/dbc.php
@@ -75,8 +75,7 @@ function dbc_admin_bar_menu() {
  * @since 0.1
  */
 function dbc_global_bar(){
-	global $blog_id;
-	
+	global $blog_id;	
 ?>
 
 <div id="global-wrapper">
@@ -125,22 +124,24 @@ function dbc_global_bar(){
  * @since 0.3
  */
 function dbc_analytics() {
-	
+
+	// Gets the site object, so we can use the domain of whatever site the user is viewing
+	$site = get_current_site();
+		
 	// If the current, logged in user is a Super Admin (i.e. has 'update_core' abilities)
 	// then don't display the script
 	if ( !current_user_can('update_core')) { ?>
 		<script type="text/javascript">
 		
-		  var _gaq = _gaq || [];
-		  _gaq.push(['_setAccount', 'UA-10285065-2']);
-		  _gaq.push(['_setDomainName', '.dentonbible.org']);
-		  _gaq.push(['_trackPageview']);
-		
-		  (function() {
-		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		  })();
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', 'UA-10285065-2']);
+			_gaq.push(['_setDomainName', '.<?php echo $site->domain ?>'],['_trackPageview'],['_trackPageLoadTime']);
+			
+			(function() {
+				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
 		
 		</script>
 	<?php
