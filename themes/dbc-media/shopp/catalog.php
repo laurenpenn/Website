2783b6a5b3060f1cd69008d9fd043eb4f181dc5d
Patrick Daly <patrick@developdaly.com>
2012-04-23 14:15:32 +0000

    Deleting old themes

--- themes/dbc-media/shopp/catalog.php
+++
@@ -1,4 +0,0 @@
-<?php shopp('catalog','featured-products','show=5&controls=false'); ?>
-<?php shopp('catalog','onsale-products','show=5&controls=false'); ?>
-<?php shopp('catalog','bestseller-products','show=5&controls=false'); ?>
-<?php shopp('catalog','new-products','show=5&controls=false'); ?>
