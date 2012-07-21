0030bf483b2c821cd24522e1d03df3eecd91467e
Patrick Daly <patrick@developdaly.com>
2012-07-21 20:10:56 +0000

    Changed header spacing issues

--- themes/dbc-child/header.php
+++ themes/dbc-child/header.php
@@ -16,7 +16,6 @@
 <meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
 <title><?php hybrid_document_title(); ?></title>
 
-<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="all" />
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
