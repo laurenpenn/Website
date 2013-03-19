b4e059e5e5cc3764587940993dc6272b4ee24d9c
Patrick Daly <patrick@developdaly.com>
2013-03-18 22:47:49 -0500

    The network_home_url will now switch domains to the current env

--- content/sunrise.php
+++ content/sunrise.php
@@ -8,6 +8,20 @@ if (isset($_SERVER['HTTP_HOST'])) {
 } elseif (isset($_SERVER['SERVER_NAME'])) {
 	$host = $_SERVER['SERVER_NAME'];
 }
+add_filter( 'login_headerurl', 'cwwp_login_logo_url' );
+/**
+ * Filters the default login URL to point to the current site's homepage.
+ *
+ * @since 1.0.0
+ * 
+ * @param string $url The default login logo URL
+ * @return string $url The amended login logo URL
+ */
+function cwwp_login_logo_url( $url ) {
+	
+	return trailingslashit( get_home_url() );
+ 
+}
 
 /**
  * This parses a domain and returns its first subdomain,
@@ -52,5 +66,7 @@ if ($_blog = $wpdb -> get_row($sql)) {
 	$current_site = $wpdb -> get_row($wpdb -> prepare("SELECT * from {$wpdb->site} WHERE id = %d", $site_id));
 	$current_site -> blog_id = $blog_id;
 
+	// Switch the network_home_url to the environment domain
 	$current_site = get_current_site_name($current_site);
+	$current_site->domain = ENV_DOMAIN;
 }
